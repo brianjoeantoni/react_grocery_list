f1487aa978681f5b7b992d0bf88269d0865f3a3a
Brian Joe Antoni <brianjoeantoni@gmail.com>
2023-07-04 12:28:46 +0700

    deploy final

--- src/App.js
+++ src/App.js
@@ -1,84 +1,85 @@
-import Header from './Header';
-import SearchItem from './SearchItem';
-import AddItem from './AddItem';
-import Content from './Content';
-import Footer from './Footer';
-import { useState, useEffect } from 'react';
-import apiRequest from './apiRequest';
+import Header from "./Header";
+import SearchItem from "./SearchItem";
+import AddItem from "./AddItem";
+import Content from "./Content";
+import Footer from "./Footer";
+import { useState, useEffect } from "react";
+import apiRequest from "./apiRequest";
 //import Exercise from './Exercise';
 
-
 function App() {
-  const API_URL = 'http://localhost:3500/items';
+  const API_URL = "http://localhost:3500/items";
 
   const [items, setItems] = useState([]);
-  const [newItem, setNewItem] = useState('');
-  const [search, setSearch] = useState('');
+  const [newItem, setNewItem] = useState("");
+  const [search, setSearch] = useState("");
   const [fetchError, setFetchError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
-  //Load data from (fake) REST Api 
-  useEffect(() => {
+  const msg = `Error: ${fetchError} \nPlease run : "npx json-server -p 3500 -w data/db.json" on VSCode`;
 
+  //Load data from (fake) REST Api
+  useEffect(() => {
     const fetchItems = async () => {
       try {
-          const response = await fetch(API_URL);
-          if(!response.ok) throw Error('Did not receive expected data');
-          const listItems = await response.json();
-          setItems(listItems);
-          setFetchError(null);
+        const response = await fetch(API_URL);
+        if (!response.ok) throw Error("Did not receive expected data");
+        const listItems = await response.json();
+        setItems(listItems);
+        setFetchError(null);
       } catch (err) {
         setFetchError(err.message);
       } finally {
         setIsLoading(false);
       }
-    }
+    };
     //just to simulate 1 seconds of waiting
     setTimeout(() => {
       fetchItems();
     }, 1000);
-  }, [])
+  }, []);
 
   const addItem = async (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
-    const myNewItem = {id, checked: false, item};
+    const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
     setItems(listItems);
 
     const postOptions = {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(myNewItem)
-    }
+      body: JSON.stringify(myNewItem),
+    };
     const result = await apiRequest(API_URL, postOptions);
     if (result) setFetchError(result);
-  }
+  };
   const handleCheck = async (id) => {
     // console.log(`key: ${id}`)
-    const listItems = items.map((item) => item.id === id ? {...item,
-      checked: !item.checked } : item)
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
     setItems(listItems);
 
-      const myItem = listItems.filter((item) => item.id === id);
-      const updateOptions = {
-        method: 'PATCH',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({checked: myItem[0].checked})
-      };
-      const reqUrl = `${API_URL}/${id}`;
-      const result = await apiRequest(reqUrl, updateOptions);
-      if (result) setFetchError(result);
+    const myItem = listItems.filter((item) => item.id === id);
+    const updateOptions = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ checked: myItem[0].checked }),
+    };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, updateOptions);
+    if (result) setFetchError(result);
   };
   const handleDelete = async (id) => {
     // console.log(id);
     const listItems = items.filter((item) => item.id !== id);
     setItems(listItems);
 
-    const deleteOptions = {method: 'DELETE'};
+    const deleteOptions = { method: "DELETE" };
     const reqUrl = `${API_URL}/${id}`;
     const result = await apiRequest(reqUrl, deleteOptions);
     if (result) setFetchError(result);
@@ -87,40 +88,35 @@ function App() {
     e.preventDefault();
     if (!newItem) return;
     addItem(newItem);
-    setNewItem('');
-  }
+    setNewItem("");
+  };
 
   return (
-    <div className='App'>
+    <div className="App">
       {/* same as <Header></Header> case sensitive */}
-      <Header title='Grocery List' /> 
-      <AddItem 
+      <Header title="Grocery List" />
+      <AddItem
         newItem={newItem}
         setNewItem={setNewItem}
         handleSubmit={handleSubmit}
       />
-      <SearchItem
-        search={search}
-        setSearch={setSearch}
-      >
-      </SearchItem>
+      <SearchItem search={search} setSearch={setSearch}></SearchItem>
       {/* <Exercise /> */}
-      <main>    
-          {isLoading === true /*no need === true*/ && <p>Loading Items...</p>}
-          {fetchError && <p style={{color: 'red'}}>{`Error: ${fetchError}`}</p>}
-        {!fetchError && !isLoading && 
-        <Content
-          items={items.filter(item => ((item.item).toLowerCase())
-            .includes(search.toLowerCase()))}
-          handleCheck={handleCheck}
-          handleDelete={handleDelete}
-        > 
-        </Content>}
+      <main>
+        {isLoading === true /*no need === true*/ && <p>Loading Items...</p>}
+        {fetchError && <div className="msg-wrapper" style={{ color: "red" }}>{msg}</div>}
+        {!fetchError && !isLoading && (
+          <Content
+            items={items.filter((item) =>
+              item.item.toLowerCase().includes(search.toLowerCase())
+            )}
+            handleCheck={handleCheck}
+            handleDelete={handleDelete}
+          ></Content>
+        )}
       </main>
-      <Footer 
-        length={items.length}
-      />
-    </div>    
+      <Footer length={items.length} />
+    </div>
   );
 }
 
